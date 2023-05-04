801ebab1f44afff3054a8256677993a203aac0d9
radek44413 <radek4441399@gmail.com>
2023-05-04 23:58:14 +0200

    b

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+function NavBar({ setActiveTab }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <ul>
+      <li><div className='nav' onClick={() => setActiveTab('home')}>Home</div></li>
+      <li><div className='nav' onClick={() => setActiveTab('education')}>Education</div></li>
+      <li><div className='nav' onClick={() => setActiveTab('experiences')}>Experiences</div></li>
+      <li><div className='nav' onClick={() => setActiveTab('projects')}>Projects</div></li>
+      <li><div className='nav' onClick={() => setActiveTab('hobby')}>Hobby</div></li>
+    </ul>
+    </>
+  )
 }
 
-export default App;
+function Home() {
+  return (<><div className='home'>Home</div></>);
+}
+
+function Education() {
+  return (<><div>Education</div></>);
+}
+
+function Experiences() {
+  return (<><div>Experiences</div></>);
+}
+
+function Projects() {
+  return (<><div>Projects</div></>);
+}
+
+function Hobby() {
+  return (<><div>Hobby</div></>);
+}
+
+export default function Container() {
+  const [activeTab, setActiveTab] = useState('home');
+
+  let content;
+  if (activeTab === 'home') {
+    content = <Home />;
+  } else if (activeTab === 'education') {
+    content = <Education />;
+  } else if (activeTab === 'experiences') {
+    content = <Experiences />;
+  } else if (activeTab === 'projects') {
+    content = <Projects />;
+  } else if (activeTab === 'hobby') {
+    content = <Hobby />;
+  }
+
+  return (
+    <>
+    <div className='portfolio'>
+      <div className='leftSide'>
+        <NavBar setActiveTab={setActiveTab} />
+      </div>
+      <div className='rightSide'>
+        {content}
+      </div>
+    </div>
+
+    </>
+  );
+}
