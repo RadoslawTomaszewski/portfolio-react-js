5cf26246aa43e4632d1f12f30447f40ce600267f
radek44413 <radek4441399@gmail.com>
2023-05-05 01:54:29 +0200

    zmiany na lepsze

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 function NavBar({ setActiveTab, activeTab }) {
   return (
+  <>
+    <img src={`${process.env.PUBLIC_URL}/me.jpg`} alt="me" />
     <ul>
       <li><div className={`nav ${activeTab === 'home' ? 'active' : ''}`} onClick={() => setActiveTab('home')}>Home</div></li>
       <li><div className={`nav ${activeTab === 'education' ? 'active' : ''}`} onClick={() => setActiveTab('education')}>Education</div></li>
@@ -11,6 +17,12 @@ function NavBar({ setActiveTab, activeTab }) {
       <li><div className={`nav ${activeTab === 'projects' ? 'active' : ''}`} onClick={() => setActiveTab('projects')}>Projects</div></li>
       <li><div className={`nav ${activeTab === 'hobby' ? 'active' : ''}`} onClick={() => setActiveTab('hobby')}>Hobby</div></li>
     </ul>
+    <div className='social'>
+    <FontAwesomeIcon className='icon' icon={faGithub} />
+    <FontAwesomeIcon className='icon' icon={faLinkedin} />
+    <FontAwesomeIcon className='icon' icon={faFacebook} />
+    </div>
+  </>
   )
 }
 
