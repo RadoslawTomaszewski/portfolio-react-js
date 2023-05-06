b3955373aa9d0a4a2f1de5d90c159c5da1d93afd
radek44413 <radek4441399@gmail.com>
2023-05-06 02:03:15 +0200

    education

--- src/App.js
+++ src/App.js
@@ -27,11 +27,57 @@ function NavBar({ setActiveTab, activeTab }) {
 }
 
 function Home() {
-  return (<><div>Home</div></>);
+  return (
+  <>
+    <div>Home</div>
+  </>
+  );
 }
 
 function Education() {
-  return (<><div>Education</div></>);
+  return (
+  <>
+    <div>
+    <div className='tile'>
+      <div className='content'>
+        <h4>2020 - 2024</h4>
+        <h2>Szkoła Główna Gospodarstwa Wiejskiego</h2>
+        <h4>Warszawa</h4>
+        <h6>Wydział Zastosowań Informatyki i Matematyki</h6>
+        <h6>Kierunek: Informatyka</h6>
+        <h6>Specjalizacja: Inżynieria Systemów Informatycznych</h6>
+      </div>
+      <div className='picture'>
+        <img src={`${process.env.PUBLIC_URL}/sggw.jpg`} alt="liceum ogolnoksztalcace" />
+      </div>
+    </div>
+
+    <div className='tile'>
+      <div className='content'>
+        <h4>2015 - 2018</h4>
+        <h2>Liceum Ogólnokształcące nr 1 im. K. I. Gałczyńskiego</h2>
+        <h4>Otwock</h4>
+        <h6>mat-fiz-inf</h6>
+        <h6>100% na egzaminie maturalnym z matematyki (część pisemna)<br/>100% na egzaminie maturalnym z języka polskiego (część ustna)</h6>
+      </div>
+      <div className='picture'>
+        <img src={`${process.env.PUBLIC_URL}/lo.jpg`} alt="liceum ogolnoksztalcace" />
+      </div>
+    </div>
+
+    <div className='tile'>
+      <div className='content'>
+        <h4>2005 - 2015</h4>
+        <h2>Publiczna Szkoła Podstawowa i Gimnazjum w Sobieniach-Jeziorach</h2>
+      </div>
+      <div className='picture'>
+        <img src={`${process.env.PUBLIC_URL}/sobienie.jpg`} alt="liceum ogolnoksztalcace" />
+      </div>
+    </div>
+    </div>
+
+  </>
+  );
 }
 
 function Experiences() {
