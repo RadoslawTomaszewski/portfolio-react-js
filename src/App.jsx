3599689b3dc3d71ba43628a1537252362633370f
radek44413 <radek4441399@gmail.com>
2023-06-17 04:56:44 +0200

    warrrr

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
   ProjectsPage,
   MVCPage,
 } from "./pages";
-import { Route, Routes } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout";
 
 // import AddTodo from "./pages/MVCPage/pages/AddTodo";
@@ -15,15 +15,17 @@ import { Layout } from "./Layout";
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="projects" element={<ProjectsPage />} />
-          <Route path="experience" element={<ExperiencePage />} />
-          <Route path="education" element={<EducationPage />} />
-          <Route path="mvc" element={<MVCPage />} />
-        </Route>
-      </Routes>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="projects" element={<ProjectsPage />} />
+            <Route path="experience" element={<ExperiencePage />} />
+            <Route path="education" element={<EducationPage />} />
+            <Route path="mvc" element={<MVCPage />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
